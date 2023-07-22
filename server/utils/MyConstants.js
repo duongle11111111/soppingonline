124126767dd92c5f9f49c231362d49bb27f2b278
duongle11111111 <139128518+duongle11111111@users.noreply.github.com>
2023-07-23 00:11:34 +0700

    a

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.sxrsszp.mongodb.net",
+  DB_USER: "duongledc",
+  DB_PASS: "123456789a",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "Duongle.dc@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "zwbssjpkiatojaqg",
+  JWT_SECRET: "a",
+  JWT_EXPIRES: "10000000", // in milliseconds
+};
+
+module.exports = MyConstants;
